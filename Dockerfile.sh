0bcadd3dec6cde795ad16c1fed42541a8621d91f
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-08 11:14:12 +0000

    Lila

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -7,7 +7,7 @@ apt-get install sudo -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 
 echo "install git"
-sudo apt-get instal git -y
+sudo apt-get install git -y
 
 echo "clone lila"
 git clone --recursive https://github.com/ornicar/lila.git
