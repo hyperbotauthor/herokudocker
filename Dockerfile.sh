d4939e570c79d6f363ec2a9594b73f38976a541b
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-09 18:53:01 +0000

    Lila

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -4,7 +4,9 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 echo "install mongodb"
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && (echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list) && apt-get update -y && apt-get install -y mongodb-org-server && mkdir -p /data/db
-sudo apt-get install mongodb-org -y
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4B7C549A058F8B6B  
+echo "deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/development multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update -y
+sudo apt-get install mongodb-org -y
 mongo --version
 #mongod &
