302e5ba852d969f4b2d1b90ef8f1af8764f09246
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-09 17:32:28 +0000

    Lila

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -1,3 +1,7 @@
+###comment this out later
+apt-get update -y && apt-get install -y gnupg2
+###
+
 echo "install mongodb"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && (echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list) && apt-get update -y && apt-get install -y mongodb-org && mkdir -p /data/db
 #mongod &
