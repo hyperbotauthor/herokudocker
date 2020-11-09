378842fba45c86716ac62af245f2d6fb1c335a78
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-09 18:59:21 +0000

    Lila

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -2,6 +2,9 @@ echo "configure tzdata"
 export TZ=Europe/Kiev
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
+echot "install wget"
+sudo apt-get install wget -y
+
 echo "install mongodb"
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && (echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list) && apt-get update -y && apt-get install -y mongodb-org-server && mkdir -p /data/db
 sudo apt-get install gnupg -y
