c6690b8ac2e1ae93dc37aefd0f6995a83e490c54
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-09 19:17:13 +0000

    Lila

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -5,6 +5,9 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 echo "install wget"
 sudo apt-get install wget -y
 
+echo "install apt-utils"
+sudo apt-get install -y apt-utils
+
 echo "install mongodb"
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && (echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list) && apt-get update -y && apt-get install -y mongodb-org-server && mkdir -p /data/db
 sudo apt-get install gnupg -y
@@ -13,9 +16,14 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongod
 sudo apt-get update -y
 sudo apt-get install -y mongodb-org
 mongo --version
-#mongod &
+
+mongod &
 
 echo "install redis"
 sudo apt-get update -y
-sudo apt-get install -y apt-utils
-sudo apt-get install redis-server -y
+sudo apt-get install redis-server -y; exit 0
+
+redis-server &
+
+cd lila
+./lila run
