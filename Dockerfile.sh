37ca05ce827a6f6d4a517ef3cfa1f4754eb4c5ef
hyperbotauthor <hyperchessbotauthor@gmail.com>
2020-11-10 13:49:06 +0000

    Gcc8

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -13,9 +13,11 @@ sudo apt-get update -y
 sudo apt-get install python2.7 -y
 echo "python version"
 python3 ––version
+python ––version
 
 echo "install gcc"
-sudo apt-get install build-essential -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get install gcc-8 g++-8
 gcc --version
 
 echo "install git"
@@ -23,12 +25,12 @@ sudo apt-get install git -y
 
 echo "install lc0"
 sudo apt-get update -y
+sudo apt-get install libstdc++-8-dev clang-6.0 ninja-build pkg-config -y
 sudo apt-get install -y ninja-build
 sudo apt-get install -y libopenblas-dev
 git clone -b release/0.23 --recurse-submodules https://github.com/LeelaChessZero/lc0.git
 cd lc0
 git clone https://github.com/mesonbuild/meson.git meson
 sed -i 's/meson /meson\/meson.py /g' build.sh
-cat build.sh
-
-./build.sh
+PATH = $PATH:~/.local/bin
+CC=clang-6.0 CXX=clang++-6.0 INSTALL_PREFIX=~/.local ./build.sh
